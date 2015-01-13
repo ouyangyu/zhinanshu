50bb8872119af1fd4bd1cf7aa8dd21f1cc384987
ouyangyu <ouyangyu_62@163.com>
2015-01-13 20:39:24 +0800

    config mysql ouyangyu

--- config/config.inc.php
+++ config/config.inc.php
@@ -9,7 +9,7 @@ return array(
 	'DB_HOST'			=>	'localhost',			// 数据库服务器地址
 	'DB_NAME'			=>	'zhinanshu',			// 数据库名
 	'DB_USER'			=>	'root',		// 数据库用户名
-	'DB_PWD'			=>	'uteach123',		// 数据库密码
+	'DB_PWD'			=>	'ouyangyu',		// 数据库密码
 
 	'DB_PORT'			=>	3306,				// 数据库端口
 	'DB_PREFIX'			=>	'ts_',		// 数据库表前缀（因为漫游的原因，数据库表前缀必须写在本文件）
